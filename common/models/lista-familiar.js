ca0a5023bc98ff9e2bf74f479569fcd16c8d3f6a
Daniel <Daniel-16-6-96@hotmail.com>
2017-11-23 10:58:23 +0100

    Asignar al usuario la lista recién creada

--- common/models/lista-familiar.js
+++ common/models/lista-familiar.js
@@ -1,11 +1,28 @@
 'use strict';
 
-module.exports = function(Listafamiliar) {
+module.exports = function (Listafamiliar) {
 
-    Listafamiliar.beforeRemote('create', function(context, Listafamiliar, next) {
-          context.args.data.Owner = context.req.accessToken.userId;
-          next();
+    Listafamiliar.beforeRemote('create', function (context, Listafamiliar, next) {
+        context.args.data.Owner = context.req.accessToken.userId;
+        next();
+    });
+
+    Listafamiliar.afterRemote('create', function (context, listafamiliar, next) {
+        var app = listafamiliar.app;
+        var a = context.req.accessToken.userId;
+
+        var Usuario = Listafamiliar.app.models.Usuario;
+        console.log(Usuario);
+
+
+        Usuario.findById(a, function(err,instance){
+
+            instance.listaFamiliarId=listafamiliar.id;
+            instance.save(function(err){
+                next();
+            });
         });
-};
-      
+        
+    });
 
+};
