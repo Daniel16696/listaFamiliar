43628bb30eacb8370193c9e7eb8e49497a005c10
Daniel <xdaxd@gmail.com>
2017-11-30 22:41:29 +0100

    Rechazar solicitudes hecho

--- common/models/usuario.js
+++ common/models/usuario.js
@@ -11,38 +11,38 @@ module.exports = function (Usuario) {
         var usuarioSolicitando = this;
 
         Usuario.findById(userId, function (err, userAuth) {
-           var  usuarioValores = userAuth.listaFamiliarId;
+            var usuarioValores = userAuth.listaFamiliarId;
 
-           // console.log(usuarioValores);
-            
+            // console.log(usuarioValores);
+
 
             usuarioSolicitando.Solicituds(function (err, listas) {
                 if (err) callback(err);
-               // console.log(listas);
+                // console.log(listas);
                 var id = listas[0].id;
-               // console.log(id);
-               if (usuarioValores==id) {
-                usuarioSolicitando.listaFamiliarId = id;
-                // console.log(usuarioSolicitando);
-                 usuarioSolicitando.save(function (err) {
-                     SalidaAceptarSolicitud = {
-                         id: id,
-                         usuarioSolicitando
-                     }
-                    // console.log(listas[0]);
-                     callback(null, SalidaAceptarSolicitud);
+                // console.log(id);
+                if (usuarioValores == id) {
+                    usuarioSolicitando.listaFamiliarId = id;
+                    // console.log(usuarioSolicitando);
+                    usuarioSolicitando.save(function (err) {
+                        SalidaAceptarSolicitud = {
+                            id: id,
+                            usuarioSolicitando
+                        }
+                        // console.log(listas[0]);
+                        callback(null, SalidaAceptarSolicitud);
 
-                     listas[0].Solicitud.remove(usuarioSolicitando, function(err){
+                        listas[0].Solicitud.remove(usuarioSolicitando, function (err) {
 
-                     });
-                 });
-                }else{
+                        });
+                    });
+                } else {
                     SalidaAceptarSolicitud = {
-                        Mensaje : "No perteneces a la lista a la que el usuario a solicitado"
+                        Mensaje: "No perteneces a la lista a la que el usuario a solicitado"
                     }
                     callback(null, SalidaAceptarSolicitud);
                 }
-              
+
 
             });
 
@@ -51,6 +51,43 @@ module.exports = function (Usuario) {
 
 
     }
+
+    /**
+     * Va a rechazar las solicitudes
+     * @param {object} context Recibe un id
+     * @param {Function(Error, array)} callback
+     */
+
+    Usuario.prototype.usuarioRechazarSolicitud = function (context, callback) {
+        var listaEliminada;
+
+        var userId = context.req.accessToken.userId;
+        var usuarioSolicitando = this;
+
+        Usuario.findById(userId, function (err, userAuth) {
+            var usuarioValores = userAuth.listaFamiliarId;
+            //console.log(userId);  //id de el usuario que está autenticado accesToken
+            // console.log(usuarioValores); //id de el usuario que está autenticado accesToken
+            // console.log(usuarioSolicitando);//todo el contenido que tiene el que está siendo solicitado
+
+            usuarioSolicitando.Solicituds(function (err, listas) {
+                if (err) callback(err);
+                console.log(listas);
+                var id = listas[0].id;
+                console.log(id);
+
+                console.log(usuarioSolicitando);
+                
+                listas[0].Solicitud.remove(usuarioSolicitando, function (err) {
+                    callback(null, listaEliminada);
+                })
+            })
+
+        });
+
+    };
+
+
 
 };
 
