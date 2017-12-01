2edad6c5cd01eddcfba8d2f4bdc3e96271477e1d
Daniel <Daniel-16-6-96@hotmail.com>
2017-12-01 09:17:41 +0100

    Realizacion de comprar todos los productos que hace que pone todos los productos a false

--- common/models/producto.js
+++ common/models/producto.js
@@ -1,5 +1,38 @@
 'use strict';
 
-module.exports = function(Producto) {
+module.exports = function (Producto) {
+    /**
+     * Va a mostrar todos los valores a false como si los hubiera comprado todos los productos
+     * @param {object} context Recibe un id
+     * @param {Function(Error, array)} callback
+     */
+
+    Producto.todosProductosComprados = function (context, callback) {
+        var producto;
+
+        var userId = context.req.accessToken.userId;
+
+        var Usuario = Producto.app.models.Usuario;
+
+
+
+        Usuario.findById(userId, function (err, usuariosBuscados) {
+         //   console.log(usuariosBuscados.listaFamiliarId);
+            var id= usuariosBuscados.listaFamiliarId;
+
+                
+                Producto.updateAll({listaFamiliarId:id},{comprar:"0"}, function(err, productos) {
+                    Producto.find({ where: { listaFamiliarId:id } }, function (err,muestraProductos) {
+                        callback(err,muestraProductos);
+                    })
+                    //console.log(productos);
+                });
+            
+
+
+        });
+        
+    };
+
 
 };
