6a92af460d227870c01f5281adc3124010a90d19
Daniel <Daniel-16-6-96@hotmail.com>
2017-12-04 08:19:16 +0100

    Funciona el metodo remoto que me ponga a false y e falta el acl y el rol dinamico

--- common/models/producto.js
+++ common/models/producto.js
@@ -9,29 +9,41 @@ module.exports = function (Producto) {
 
     Producto.todosProductosComprados = function (context, callback) {
         var producto;
-
         var userId = context.req.accessToken.userId;
-
         var Usuario = Producto.app.models.Usuario;
 
-
-
         Usuario.findById(userId, function (err, usuariosBuscados) {
-         //   console.log(usuariosBuscados.listaFamiliarId);
-            var id= usuariosBuscados.listaFamiliarId;
-
-                
-                Producto.updateAll({listaFamiliarId:id},{comprar:"0"}, function(err, productos) {
-                    Producto.find({ where: { listaFamiliarId:id } }, function (err,muestraProductos) {
-                        callback(err,muestraProductos);
-                    })
-                    //console.log(productos);
-                });
-            
-
-
+            //   console.log(usuariosBuscados.listaFamiliarId);
+            var id = usuariosBuscados.listaFamiliarId;
+            Producto.updateAll({ listaFamiliarId: id }, { comprar: "0" }, function (err, productos) {
+                Producto.find({ where: { listaFamiliarId: id } }, function (err, muestraProductos) {
+                    callback(err, muestraProductos);
+                })
+                //console.log(productos);
+            });
         });
-        
+    };
+
+    /**
+    * poner el atributo comprar a false y viceversa
+    * @param {object} context Se necesita pasarle un id
+    * @param {Function(Error, array)} callback
+    */
+
+    Producto.prototype.comprarUnDeterminadoProducto = function (context, callback) {
+        var producto;
+        var productoElegido = this;
+        //console.log(productoElegido);
+        //console.log(productoElegido.comprar);
+        productoElegido.comprar = !productoElegido.comprar;
+        productoElegido.save(function (err) {
+            Producto.find({ where: { listaFamiliarId: productoElegido.listaFamiliarId } }, function (err, producto) {
+                console.log(producto);
+                callback(null, producto);
+            });
+        });
+
+
     };
 
 
